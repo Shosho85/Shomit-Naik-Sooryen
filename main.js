224855798dd8057141fa333afa83e67e1b96769e
shosho85 <naikshomit@gmail.com>
2019-02-27 14:00:00 -0500

    Completed the first two of the code assignment and partial of the 3rd.

--- main.js
+++ main.js
@@ -3,10 +3,19 @@
 // 1. Make each item's paragraph collapse/expand when the title is clicked
 
 
-
+$(".item").click(function(e) {
+	e.preventDefault();
+	// $(".description").toggle();
+	$(".description", this).toggle();
+});
 
 
 // 2. Remove each item when we click on the closing X
+
+$(".close").click(function(e) {
+	e.preventDefault();
+	$(this).closest(".item").remove()
+});
 
 
 
@@ -16,7 +25,15 @@
 //    The title of the new item has to match the input value
 //    The new item should also have the same properties (collapse/expand and close) as the other items
 
+function appendText() {
+	// var txt1 = "<p>Text</p>";
+	// var txt2 = $("<div class='item'></div>").html('<h1 class="title">Title 1</h1><span class="close">x</span><p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id scelerisque lacus, rutrum lobortis nisl. Ut dapibus, quam in auctor pharetra, nibh erat pharetra velit, vitae tristique mauris dui quis.</p>');
+	var txt3 = $('input[name=newtitle]').val();
 
+	// $(".item-list").append("<div class='item'>" + txt3 + "</div>");
+
+	$(".item-list").append('<div class="item"><h1 class="title">'+ txt3 +'</h1><span class="close">x</span><p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id scelerisque lacus, rutrum lobortis nisl. Ut dapibus, quam in auctor pharetra, nibh erat pharetra velit, vitae tristique mauris dui quis.</p></div>');
+}
 
 
 //    Optional: 
